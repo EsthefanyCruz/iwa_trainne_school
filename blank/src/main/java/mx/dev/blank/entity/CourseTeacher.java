f521d792e357608a6e0ffff1f651e4394c2feb3d
Esthefany Cruz <scruz.developer@gmail.com>
2021-03-26 20:37:40 -0600

    Loading base project

---
+++ blank/src/main/java/mx/dev/blank/entity/CourseTeacher.java
@@ -0,0 +1,55 @@
+package mx.dev.blank.entity;
+
+import javax.persistence.FetchType;
+import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "course_teacher")
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CourseTeacher implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  private int id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "course_id", nullable = false)
+  private Course course;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "teacher_id", nullable = false)
+  private Teacher teacher;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "room_id", nullable = false)
+  private Room room;
+
+  @Column(name = "start_time", nullable = false)
+  private LocalTime startTime;
+
+  @Column(name = "end_time", nullable = false)
+  private LocalTime endTime;
+
+  @Column(name = "day", nullable = false)
+  private String day;
+
+}
---
+++ blank/src/main/java/mx/dev/blank/entity/Student.java
@@ -0,0 +1,49 @@
+package mx.dev.blank.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "student")
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Student implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  private int id;
+
+  @Column(name = "uuid", nullable = false)
+  private String uuid;
+
+  @Column(name = "name", nullable = false)
+  private String name;
+
+  @Column(name = "first_surname", nullable = false, columnDefinition = "TEXT")
+  private String firstSurname;
+
+  @Column(name = "second_surname", columnDefinition = "TEXT")
+  private String secondSurname;
+
+  @Temporal(TemporalType.DATE)
+  @Column(name = "birthday", nullable = false)
+  private Date birthday;
+
+}
